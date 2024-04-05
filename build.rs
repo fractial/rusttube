112b86ecae477020aa2e9200692e6f1b5f75cac1
fractial <timo.staab@outlook.com>
2024-04-05 15:26:38 +0200

    Added playlist support

---
+++ build.rs
@@ -0,0 +1,9 @@
+// extern crate winres::WindowsResource;
+
+fn main() {
+    // if cfg!(target_os = "windows") {
+        // let mut res = WindowsResource::new();
+        // res.set_icon("favicon.ico");
+        // res.compile().unwrap();
+    // }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,11 @@ use crate::request::{get_request, ResponseType::Stream};
 
 mod request;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    let id = &args[1];
+async fn download(id: &str) -> Result<(), Box<dyn Error>> {
     let url = format!("https://piped-api.lunar.icu/streams/{}", id);
-
     println!("{}", url);
 
     let result = get_request(&url, None).await?;
-
     let body = String::from_utf8_lossy(&result).to_string();
 
     let parsed: Value = from_str(&body).expect("Failed to parse JSON");
@@ -25,12 +20,48 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let stream_url = parsed["audioStreams"][3]["url"].as_str().unwrap_or("default");
 
     let mut download_dir = env::var("USERPROFILE").expect("Failed to find `HOME` directory");
-    download_dir.push_str("/Downloads");
+    download_dir.push_str("/Music");
     let file_path = format!("{}/{}.mp4", download_dir, name);
     let mut file = File::create(file_path).expect("Error creating file");
 
     let stream = get_request(&stream_url, Some(Stream)).await?;
     file.write_all(&stream)?;
+
+    Ok(())
+}
+
+async fn playlist(id: &str) -> Result<(), Box<dyn Error>> {
+    let url = format!("https://piped-api.lunar.icu/playlists/{}", id);
+    println!("{}", url);
+
+    let result = get_request(&url, None).await?;
+    let body = String::from_utf8_lossy(&result).to_string();
+
+    let parsed: Value = from_str(&body).expect("Failed to parse JSON");
+    let streams = parsed["relatedStreams"].as_array().expect("Failed to parse `relatedStreams`");
+
+    for stream in streams {
+        let stream_uri = stream["url"].to_string();
+        let stream_url = stream_uri.trim_matches('"').to_string();
+        let stream_id = stream_url.replace("/watch?v=", "");
+        let _ = download(&stream_id).await;
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let id = &args[1];
+
+    if args.contains(&String::from("--playlist")) || args.contains(&String::from("-p")) {
+        let _ = playlist(id).await;
+
+        return Ok(());
+    }
+
+    let _ = download(&id).await;
 
     Ok(())
 }
