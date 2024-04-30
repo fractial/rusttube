54f1620e4069ba5232b4e1c5d6a1f13e3c27a3a5
fractial <timo.staab@outlook.com>
2024-04-30 21:53:03 +0200

    Added search

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::error::Error;
 use std::fs::File;
-use std::io::Write;
+use std::io::{stdin, Write};
 use std::string::ToString;
 use std::time::Instant;
 use regex::Regex;
@@ -10,6 +10,8 @@ use request::get_request;
 use tokio;
 
 mod request;
+
+const URL: &str = "https://pipedapi.kavin.rocks";
 
 fn get_file_name(file_name: &str) -> String {
     let pattern = Regex::new(r#"[<>:"/\\|?*]"#).unwrap();
@@ -36,7 +38,7 @@ async fn download(url_instance: &str, id: &str) -> Result<(), Box<dyn Error>> {
 
     let mut dir = env::var("USERPROFILE")?;
     dir.push_str("/Music");
-    let file_type = "mp4";
+    let file_type = "mp3";
     let file_path = format!("{}/{}.{}", dir, file_name, file_type);
     let mut file = File::create(file_path).expect("Error creating file");
     file.write_all(&stream)?;
@@ -64,16 +66,54 @@ async fn download_playlist(url_instance: &str, id: &str) -> Result<(), Box<dyn E
     Ok(())
 }
 
+fn read_index() -> usize {
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("");
+
+    let index: usize = input.trim().parse().expect("");
+
+    return index;
+}
+
+async fn search(url_instance: &str, search_query: &str) -> Result<(), Box<dyn Error>> {
+    let mut url = String::from(url_instance);
+    url.push_str(format!("/search?q=\"{}\"&filter=all", search_query).as_str());
+
+    let response = get_request(&url).await?;
+    let body = response.text().await?;
+    let data: Value = from_str(&body)?;
+    let items = data["items"].as_array().expect("");
+
+    for (index, item) in items.iter().enumerate() {
+        let title = item["title"].to_string();
+        println!("[{}] {}", index, title);
+    }
+
+    let index = read_index();
+    let id = items[index]["url"].to_string().replace("\"", "").replace("/watch?v=", "");
+    download(url_instance, &id).await?;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut url = "https://pipedapi.kavin.rocks";
+    let mut url = URL;
     let args: Vec<String> = env::args().collect();
     let id = &args[1];
 
     if let Some(index) = args.iter().position(|arg| arg == "--instance" || arg == "-i") {
         let url_instance = &args[index + 1];
         if !url_instance.is_empty() {
-            url = url_instance;
+            url = url_instance.strip_suffix("/").unwrap_or(&url_instance);
+        }
+    }
+
+    if let Some(index) = args.iter().position(|arg| arg == "--search" || arg == "-s") {
+        let search_query = &args[index + 1];
+        if !search_query.is_empty() {
+            search(&url, search_query).await?;
+            return Ok(());
         }
     }
 
